33a77ae5a2b5e1c13548e7a7de111d3d16cb1146
songmingwu6-web <songmingwu6@gmail.com>
2026-04-15 23:08:00 +0800

    主框架2

--- new_project_2026.4.6/game_function.py
+++ new_project_2026.4.6/game_function.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from ship import Ship
 def check_events():
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -12,4 +13,19 @@ def update_screen(ai_settings,screen,ship):
     ship.blitme()
     #r让最近绘制的屏幕可见
     pygame.display.flip()
+def check_events(ship):
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            sys.exit()
+        elif event.type ==pygame.KEYDOWN:
+         if event.key == pygame.K_RIGHT:
+            ship.moving_right = True
+         elif event.key == pygame.K_LEFT:
+             ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+         if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+         elif event.key ==pygame.K_LEFT:
+             ship.moving_left = False
     
