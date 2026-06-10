00ce5a1cfca653e8f8b55f716d5a107001425183
songmingwu6-web <songmingwu6@gmail.com>
2026-06-10 22:56:38 +0800

    ``` feat(matplotlib): 更新散点图示例代码
    将静态的固定数据改为动态生成的数值范围，
    从简单的5个点扩展到1-1000的数值范围，
    并调整了散点大小和坐标轴显示范围以适应新的数据规模
    ```

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-x_values = [1,2,3,4,5]#x轴数据
-y_values = [1,4,9,16,25]#y轴数据
-plt.scatter(x_values,y_values,s=100)#s参数设置散点的大小
+x_values = list(range(1,1001))#创建x轴数据
+y_values = [x**2 for x in x_values]#y轴数据
+plt.scatter(x_values,y_values,s=40)#s参数设置散点的大小
 #设置图表标题并给坐标轴加上标签
 plt.title("Squares Numbers",fontsize = 24)
 plt.xlabel("Value",fontsize = 14)
 plt.ylabel("Square of Value",fontsize = 14)
 #设置刻度标记的大小
-plt.tick_params(axis = 'both',which = 'major',labelsize = 14)
+plt.axis([0,1100,0,1100000])#设置每个坐标的取值范围
 plt.show()
