d91c6078637172c4bfb7e0f117d256704e0cb11c
songmingwu6-web <songmingwu6@gmail.com>
2026-06-15 22:30:58 +0800

    ``` feat(random_walk): 添加随机漫步路径填充功能
    实现了fill_walk方法来计算随机漫步过程中包含的所有坐标点，
    通过随机选择方向和距离来模拟漫步过程，并避免原地踏步的情况。
    ```

---
+++ matplotlib/rw_visual.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# 创建一个 RandomWalk 实例，并将其绘制出来
+rw = RandomWalk()
+rw.fill_walk()
+plt.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
